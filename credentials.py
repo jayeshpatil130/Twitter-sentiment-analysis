20cde240d65b77bd73ad2a38190d48946c70bc0b
Jayesh Patil <jayeshpatil130@gmail.com>
2020-08-01 18:07:20 -0400

    Commit

---
+++ credentials.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+API_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+API_SECRET_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+ACCESS_TOEKN = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+ACCESS_TOKEN_SECRET = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+
+CONNECTION_STRING = "sqlite:///tweets.db"
+
+
